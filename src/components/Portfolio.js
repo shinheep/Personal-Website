ed2012a2398d41b5f026e0545e4584aa43c7a53d
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-11-30 23:39:41 -0800

    fixed wrap, and added project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -30,6 +30,18 @@ const Portfolio = () => {
                 </div>
 
                 <div className="petBuddies">
+                    <h3 className="projectTitle">Dictionary</h3>
+                    <div className="projectDescription">A React.js app, built on a whim one free afternoon. A simple, but full stack dictionary, pulling data from the Dictionary API. As a dictionary does, the user can search a word to look up its definition. Programming is fun! </div>
+                    <img src="https://res.cloudinary.com/dxqwpud0l/image/upload/v1638344170/Screen_Shot_2021-11-30_at_11.35.51_PM_bthbjz.png" alt="plant" className="projectPic"/>
+                    
+                    <div className="projectButtons">
+                        <a target="blank" className="liveLinkButton" href="https://shinheep.github.io/dictionary/"><input className="linkButton" type="submit" value="Live Link"/></a>
+
+                        <a target="blank" className="liveLinkButton" href="https://github.com/shinheep/dictionary"><input className="linkButton" type="submit" value="GitHub Link"/></a>
+                    </div>
+                </div>
+
+                <div className="petBuddies">
                     <h3 className="projectTitle">Leafy</h3>
                     <div className="projectDescription">Collaborated with a team of developers to create a full stack app with MERN. Users can add plants to their list, browse through their plants, water them, and remove them from their list.</div>
                     <img src="https://res.cloudinary.com/dxqwpud0l/image/upload/v1638085698/Screen_Shot_2021-11-27_at_11.47.36_PM_klt4bz.png" alt="plant" className="projectPic"/>

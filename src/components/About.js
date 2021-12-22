c6dabfcb309b823aa7cf649e7a1c40f5fb048a00
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-12-22 15:06:40 -0800

    CSS changes

--- src/components/About.js
+++ src/components/About.js
@@ -9,16 +9,18 @@ const About = () => {
                 <div className="sectionSeparator"></div>
                 
                 <div className="aboutMain">
-                    <div className="paragraph">
-                        Hello! My name is Shinhee, and I am a full-stack software engineer creating modern and responsive design for Mobile apps, and the Web. My interest in web development started back in 2010 when I decided my Xanga page needed to have pretty stars flowing down the front page -- turns out that taught me a lot about HTML & CSS!
-                    </div>
+                    <div className="insideAbout">
+                        <div className="paragraph">
+                            Hello! My name is Shinhee, and I am a full-stack software engineer creating modern and responsive design for Mobile apps, and the Web. My interest in web development started back in 2010 when I decided my Xanga page needed to have pretty stars flowing down the front page -- turns out that taught me a lot about HTML & CSS!
+                        </div>
 
-                    <div className="paragraph">
-                        Fast-forward to today, I have just completed the Software Engineering Immersive Program at General Assembly, and I am proud to say my knowledge of programming goes beyond adding pretty stars to a website. With a detail-oriented approach to solving design and development problems, I focus on adding value to any team that I join. I utilize full-stack software solutions primarily focused on JavaScript, React, Node.Js, Python, Django, and MongoDB to create dynamic, high-quality user-focused solutions that deliver the best experience using the highest quality modern development methodology.
-                    </div>
+                        <div className="paragraph">
+                            Fast-forward to today, I have just completed the Software Engineering Immersive Program at General Assembly, and I am proud to say my knowledge of programming goes beyond adding pretty stars to a website. With a detail-oriented approach to solving design and development problems, I focus on adding value to any team that I join. I utilize full-stack software solutions primarily focused on JavaScript, React, Node.Js, Python, Django, and MongoDB to create dynamic, high-quality user-focused solutions that deliver the best experience using the highest quality modern development methodology.
+                        </div>
 
-                    <div className="paragraph">
-                        Apart from coding, I'm a proud dog and plant mom!
+                        <div className="paragraph">
+                            Apart from coding, I'm a proud dog and plant mom!
+                        </div>
                     </div>
                     
                 </div>

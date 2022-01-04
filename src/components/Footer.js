d4a4c5e6f2d9bb24fabcc30c9db13279fedf79e7
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2022-01-03 21:59:58 -0800

    Updated date in footer to 2022

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="footer">
-            <footer>© 2021 Shinhee Park.</footer>
+            <footer>© 2022 Shinhee Park.</footer>
             <a target='blank' href='https://www.linkedin.com/in/shinhee-park/'><div className="socials"><i class="fab fa-linkedin"></i></div></a>
             <a target='blank' href='https://github.com/shinheep'><div className="socials"><i class="fab fa-github"></i></div></a>
             <a target='blank' href='https://twitter.com/shinheecodey'><div className="socials"><i class="fab fa-twitter-square"></i></div></a>
